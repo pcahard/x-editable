0488b13ad038026683f801363305c505422119ef
mjiroff <marion.jiroff@geolocalisation-sabatier.fr>
2016-10-27 14:53:33 +0200

    Success button for validation

--- src/editable-form/editable-form-bootstrap.js
+++ src/editable-form/editable-form-bootstrap.js
@@ -30,7 +30,7 @@ Editableform based on Twitter Bootstrap 2
     });    
     
     //buttons
-    $.fn.editableform.buttons = '<button type="submit" class="btn btn-primary editable-submit"><i class="icon-ok icon-white"></i></button>'+
+    $.fn.editableform.buttons = '<button type="submit" class="btn btn-success editable-submit"><i class="icon-ok icon-white"></i></button>'+
                                 '<button type="button" class="btn editable-cancel"><i class="icon-remove"></i></button>';         
     
     //error classes
